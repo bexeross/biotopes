c5790fd22029b88903fd5f50e4fa8325f0ea3f2c
Gonzalez-Mirelis <genoveva@hi.no>
2021-09-30 10:41:37 +0200

    added a config file to keep paths separate from other code

---
+++ config.R
@@ -0,0 +1,6 @@
+
+delphi_prosjekt_drive <- "U:" # which letter have you mapped this drive to?
+
+dataPath <- file.path(delphi_prosjekt_drive,"Mareano/VIDEOLAB/VIDEO DATA/200m_scale_species_by_sample/Data_Delivery_2021")
+outPath <- file.path(dataPath,"outputs")
+codePath <- getwd()
