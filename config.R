be6d8118dd5763cc6382832a0c39ca04075fa0eb
Gonzalez-Mirelis <genoveva@hi.no>
2021-10-06 13:20:15 +0200

    added a function to set a crs to WGS84 and transform it to UTM33N

--- config.R
+++ config.R
@@ -4,3 +4,12 @@ delphi_prosjekt_drive <- "U:" # which letter have you mapped this drive to?
 dataPath <- file.path(delphi_prosjekt_drive,"Mareano/VIDEOLAB/VIDEO DATA/200m_scale_species_by_sample/Data_Delivery_2021")
 outPath <- file.path(dataPath,"outputs")
 codePath <- getwd()
+
+## Some functions
+
+utmize <- function(x){
+  require(rgdal)
+  proj4string(x)=CRS("+init=epsg:4326")
+  result <- spTransform(x, CRS("+init=epsg:32633")) 
+  return(result)
+}
