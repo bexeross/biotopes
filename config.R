ada9526b34ed8319c69a1119cd5f0c49c666750f
Gonzalez-Mirelis <genoveva@hi.no>
2021-10-11 15:50:56 +0200

    added a new function to make a class report and improved the annotations on another function

--- config.R
+++ config.R
@@ -7,9 +7,25 @@ codePath <- getwd()
 
 ## Some functions
 
+#convert geographic coordinates in WGS84 to UTM33N
+
 utmize <- function(x){
   require(rgdal)
   proj4string(x)=CRS("+init=epsg:4326")
   result <- spTransform(x, CRS("+init=epsg:32633")) 
   return(result)
+}
+
+#Compute mean abundance of all species present in all samples of a given class in a given classification
+
+classreport <- function(data, classifications, level, class){
+  aa <- paste0("level",level)
+  id <- classifications %>% 
+    filter(get(aa)==class) %>%
+    select(SampID) %>%
+    pull()
+  spp <- data %>% filter(SampID %in% id) %>%
+    select(-1) %>%
+    select_if(colSums(.)>0)
+  return(sort(colMeans(spp), decreasing = TRUE))
 }
